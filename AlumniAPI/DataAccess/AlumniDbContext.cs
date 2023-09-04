577435e29e15a3ded0f4f243b3798787695803c2
Simon Saberian <simon.saberian@gmail.com>
2023-09-04 09:33:49 +0200

    ✨ feat: Add models to db context and seed data

--- AlumniAPI/DataAccess/AlumniDbContext.cs
+++ AlumniAPI/DataAccess/AlumniDbContext.cs
@@ -10,8 +10,20 @@ public class AlumniDbContext: DbContext
     }
     
     public DbSet<Test> Test { get; set; }
+    public DbSet<User> User { get; set; }
+    public DbSet<Group> Group { get; set; }
+    public DbSet<Post> Post { get; set; }
+    public DbSet<EventInfo> EventInfo { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<User>()
+            .HasMany(e => e.Posts)
+            .WithOne(e => e.Creator)
+            .HasForeignKey(e => e.CreatorId)
+            .IsRequired();
+        
         modelBuilder.Entity<Test>().HasData(SeedHelper.GetTestSeeds());
+        modelBuilder.Entity<Post>().HasData(SeedHelper.GetPostSeeds());
+        modelBuilder.Entity<EventInfo>().HasData(SeedHelper.GetEventInfoSeeds());
     }
 }
