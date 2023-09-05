4a613e3ff084f30037f4bccc111eb2517814dd7f
Simon Saberian <simon.saberian@gmail.com>
2023-09-05 14:11:58 +0200

    ✨ feat: Add http context extension methods

---
+++ AlumniAPI/Extensions/HttpContextExt.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+
+namespace AlumniAPI.Extensions;
+
+public static class HttpContextExt
+{
+    public static string GetUserEmail(this HttpContext httpContext)
+    {
+        return httpContext.User.Claims.SingleOrDefault(x => x.Type == ClaimTypes.Email)?.Value ?? string.Empty;
+    }
+    public static string GetUserName(this HttpContext httpContext)
+    {
+        return httpContext.User.Claims.Single(x => x.Type == "name").Value;
+    }
+}
