d8ec18c55ba527a5435b54fae124e14683084d31
Oscar <oscar.nordstrom2000@gmail.com>
2023-09-05 08:31:08 +0200

    🐛fix: removing id from createDto

--- AlumniAPI/DTOs/User/CreateUserDto.cs
+++ AlumniAPI/DTOs/User/CreateUserDto.cs
@@ -2,7 +2,6 @@
 
 public class CreateUserDto
 {
-    public int Id { get; set; }
     public string Name { get; set; }
     public string? AvatarURL { get; set; }
     public string? Bio { get; set; }
