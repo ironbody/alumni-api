30f46540a4f59f10b34dda0c563ec9c6a83c7990
Oscar <oscar.nordstrom2000@gmail.com>
2023-09-11 11:41:00 +0200

    🐛fix: update user DTO not including email. Email is not an option

--- AlumniAPI/DTOs/User/UpdateUserDto.cs
+++ AlumniAPI/DTOs/User/UpdateUserDto.cs
@@ -4,6 +4,7 @@ public class UpdateUserDto
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string Email { get; set; }
     public string? AvatarURL { get; set; }
     public string? Bio { get; set; }
     public string? FunFact { get; set; }
