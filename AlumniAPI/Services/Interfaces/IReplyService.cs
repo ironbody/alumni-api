b9f0f48d722e6f485daba8cdd668ca527107d9c2
Simon Saberian <simon.saberian@gmail.com>
2023-09-15 13:47:29 +0200

    💡 chore: Add comments to reply service interface

--- AlumniAPI/Services/Interfaces/IReplyService.cs
+++ AlumniAPI/Services/Interfaces/IReplyService.cs
@@ -5,5 +5,12 @@ namespace AlumniAPI.Services.Interfaces;
 
 public interface IReplyService : IRepository<Reply>
 {
+    /// <summary>
+    /// Finds all replies that are visible to the user that contain the search query.
+    /// Case insensitive.
+    /// </summary>
+    /// <param name="userId">User's id</param>
+    /// <param name="query">String that should be searched for</param>
+    /// <returns>List of found replies</returns>
     Task<IEnumerable<Reply>> SearchRepliesVisibleToUser(int userId, string query);
 }
