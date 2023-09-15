15c3925c539e83c1054ea1d59bdae282bb62ffc9
Simon Saberian <simon.saberian@gmail.com>
2023-09-15 13:46:55 +0200

    💡 chore: Add comments to post service interface

--- AlumniAPI/Services/Interfaces/IPostService.cs
+++ AlumniAPI/Services/Interfaces/IPostService.cs
@@ -5,7 +5,27 @@ namespace AlumniAPI.Services.Interfaces;
 
 public interface IPostService : IRepository<Post>
 {
+    /// <summary>
+    /// Retrieves all posts from the groups the user is in.
+    /// </summary>
+    /// <param name="userId">User's Id</param>
+    /// <returns>List of found posts</returns>
     Task<IEnumerable<Post>> GetAllPostsVisibleToUser(int userId);
+
+    /// <summary>
+    /// Finds all posts that is visible to the user containing the search string in its title or body.
+    /// Case insensitive.
+    /// </summary>
+    /// <param name="userId"></param>
+    /// <param name="query"></param>
+    /// <returns></returns>
     Task<IEnumerable<Post>> SearchPostsVisibleToUser(int userId, string query);
+
+    /// <summary>
+    /// Checks if the user is in the group that the post is posted to
+    /// </summary>
+    /// <param name="postId">Post's Id</param>
+    /// <param name="userId">User's Id</param>
+    /// <returns>True if the user is in same group as the post. False otherwise.</returns>
     Task<bool> PostIsVisibleToUser(int postId, int userId);
 }
